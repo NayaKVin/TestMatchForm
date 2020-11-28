b89fb3297e709bf628d43c7618a04455da85e2e4
NayaKVin <vinayak_kumbhar@hotmail.com>
2020-11-28 14:11:03 +0530

    Initial CommitCreated New Project Added Controller and Model class

---
+++ TestMatchForm/Controllers/MatchFormController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMatchForm.Models;
+using MySql.Data.MySqlClient;
+
+namespace TestMatchForm.Controllers
+{
+    public class MatchFormController : Controller
+    {
+        // GET: MatchForm
+        public ActionResult TestMchForm()
+        {
+            return View();
+        }
+
+        public JsonResult Insertplayers(PlayerModel MachDta)
+        {
+            int MatchId = 1; // we can write logic for match ID
+
+            string constr = ConfigurationManager.ConnectionStrings["TestCon"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(constr))
+            {
+                string query = "insert into testmatch.tbl_match (MatchId, MatchName, matchDate, team1, team2, StartTime, Address) value('" + MatchId + "','" + MachDta.MName + "','" + MachDta.Mdate + "','" + MachDta.TeamOne + "','" + MachDta.Teamtwo + "','" + MachDta.MTime + "','" + MachDta.MAddr + "')";
+
+                //Response.Write(query);
+                using (MySqlCommand cmmd = new MySqlCommand(query))
+                {
+                    cmmd.Connection = conn;
+                    conn.Open();
+                    try { cmmd.ExecuteNonQuery(); }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                }
+                conn.Close();
+            }
+
+
+            string plyrNm = "", Batsman = "", PlyrPostion = "", PlyrCnty = "";
+            string[] tblRow = MachDta.MPlyr.Split('|');
+            foreach (string RoCOl in tblRow)
+            {
+                int i = 0;
+                string[] RC = RoCOl.Split('`');
+                foreach (string cel in RC)
+                {
+                    if (i == 0) { plyrNm = cel; }
+                    if (i == 1) { Batsman = cel; }
+                    if (i == 2) { PlyrPostion = cel; }
+                    if (i == 3) { PlyrCnty = cel; }
+                    i++;
+
+                }
+
+                using (MySqlConnection conn = new MySqlConnection(constr))
+                {
+                    string query = "insert into testmatch.match_players(MatchId, PlyrName, PlyrType, PlyrPostion, team) value('" + MatchId + "','" + plyrNm + "','" + Batsman + "','" + PlyrPostion + "','" + PlyrCnty + "')";
+
+                    using (MySqlCommand cmmd = new MySqlCommand(query))
+                    {
+                        cmmd.Connection = conn;
+                        conn.Open();
+
+                        try { cmmd.ExecuteNonQuery(); }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+                    conn.Close();
+                }
+
+            }
+
+
+
+
+            return Json("Success");
+        }
+    }
+}
---
+++ TestMatchForm/Models/PlayerModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMatchForm.Models
+{
+    public class PlayerModel
+    {
+        public string MatchId { get; set; }
+        public string MName { get; set; }
+        public string TeamOne { get; set; }
+        public string Teamtwo { get; set; }
+        public DateTime Mdate { get; set; }
+        public DateTime MTime { get; set; }
+        public string MAddr { get; set; }
+        public string plyrNm { get; set; }
+        public string PlyrType { get; set; }
+        public string PlyrPostion { get; set; }
+        public string PlyrCnty { get; set; }
+        public string MPlyr { get; set; }
+
+        //public List<PlayerTbl> MPlyr { get; set; }
+    }
+
+    public class PlayerTbl
+    {
+        public string plyrNm { get; set; }
+        public string PlyrType { get; set; }
+        public string PlyrPostion { get; set; }
+        public string PlyrCnty { get; set; }
+    }
+}
